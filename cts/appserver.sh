206524261d38c49b1f8d96903681e6e58d035236
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-06-15 14:02:16 +0100

    appserver build

---
+++ cts/appserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum install -y wget
+
+echo "use prebuilt wildfly zip $preBuiltAppServerZip"
+mkdir -p build/target
+mkdir wf
+cd wf
+wget --no-check-certificate "$preBuiltAppServerZip" --output-document=wildfly.zip
+unzip -q wildfly.zip
+rm wildfly.zip
+mv * tempname
+mv tempname wildfly-prebuilt
+zip -r ../build/target/wildfly.zip .
+cd ..
+ls
+echo "custom prebuilt WildFly zip" > build.name
+echo "pulling $preBuiltAppServerZip" > build.txt
+exit 0;
