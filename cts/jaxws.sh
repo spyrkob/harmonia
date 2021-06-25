96a62a11309ce54712b2028b577850ab4e23d2a2
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-06-25 08:57:57 +0100

    add ps and netstat

--- cts/jaxws.sh
+++ cts/jaxws.sh
@@ -2,6 +2,8 @@
 
 source props.sh
 
+dnf install -y procps-ng net-tools 
+
 # sample cpu/memory every 30 seconds, for about 60 hours (really until end of job run), write to console in background, kill any currently running sar commands, so we don't clutter console too much
 #killall sar || true
 #sar -r -u -d -b 30 14400 &
